e006be11085072b86138ae1fb7a16636d7e64caf
Helena Flausino <helenaflausino@gmail.com>
2025-03-31 22:32:48 -0300

    fit(ap09): implementa exercicio 1.1.

---
+++ script.sql
@@ -0,0 +1,44 @@
+--Nota. Para cada exercício, produza duas soluções: uma que utilize apenas IF e suas
+--variações e outra que use apenas CASE e suas variações.
+--Nota. Para cada exercício, gere valores aleatórios conforme a necessidade. Use a função
+--do Bloco de Código 1.1.
+
+
+--1.1 Faça um programa que exibe se um número inteiro é múltiplo de 3.
+--IF
+DO $$
+DECLARE
+    n1 INT;
+BEGIN
+    n1 := valor_aleatorio_entre(1,30);
+    IF n1 % 3 = 0 THEN
+        RAISE NOTICE '% é múltiplo de 3 ', n1::INT;
+    ELSE
+        RAISE NOTICE '% não é múltiplo de 3 ', n1::INT;
+    END IF;
+END
+$$;
+
+--CASE
+DO $$
+DECLARE
+    n1 INT;
+BEGIN
+    n1 := valor_aleatorio_entre(1,30);
+    CASE
+        WHEN n1 % 3 = 0 THEN
+            RAISE NOTICE '% é múltiplo de 3 ', n1::INT;
+        ELSE
+            RAISE NOTICE '% não é múltiplo de 3 ', n1::INT;
+    END CASE;
+END
+$$;
+
+--1.1 Bloco de Código 1.1.
+CREATE OR REPLACE FUNCTION valor_aleatorio_entre (lim_inferior INT, lim_superior
+INT) RETURNS INT AS
+$$
+BEGIN
+RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
+END;
+$$ LANGUAGE plpgsql;
