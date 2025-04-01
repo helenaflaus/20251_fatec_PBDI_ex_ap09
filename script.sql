cd40bd26c560715ca332dab19a86a2378bac720e
Helena Flausino <helenaflausino@gmail.com>
2025-03-31 23:00:10 -0300

    fit(ap09): implementa exercicio 1.3

--- script.sql
+++ script.sql
@@ -3,6 +3,89 @@
 --Nota. Para cada exercício, gere valores aleatórios conforme a necessidade. Use a função
 --do Bloco de Código 1.1.
 
+--1.4 Um comerciante comprou um produto e quer vendê-lo com um lucro de 45% se o valor
+--da compra for menor que R$20. Caso contrário, ele deseja lucro de 30%. Faça um
+--programa que, dado o valor do produto, calcula o valor de venda.
+
+
+--1.3 Faça um programa que opera de acordo com o seguinte menu.
+--Opções:
+--1 - Soma
+--2 - Subtração
+--3 - Multiplicação
+--4 - Divisão
+--Cada operação envolve dois números inteiros. O resultado deve ser exibido no formato
+--op1 op op2 = res
+--Exemplo:
+--2 + 3 = 5
+--IF
+DO $$
+DECLARE
+    op1 INT := valor_aleatorio_entre(1,30);  
+    op2 INT := valor_aleatorio_entre(1,30);   
+    opcao INT := 4; -- (1: soma, 2: subtração, 3: multiplicação, 4: divisão)
+    resultado NUMERIC (10, 2);
+    operador TEXT;
+BEGIN
+    IF opcao = 1 THEN
+        resultado := op1 + op2;
+        operador := '+';
+    ELSIF opcao = 2 THEN
+        resultado := op1 - op2;
+        operador := '-';
+    ELSIF opcao = 3 THEN
+        resultado := op1 * op2;
+        operador := '*';
+    ELSIF opcao = 4 THEN
+        IF op2 = 0 THEN
+            RAISE NOTICE 'Erro: divisão por zero não é permitida';
+            RETURN;
+        END IF;
+        resultado := op1 / op2 ::FLOAT;
+        operador := '/';
+    ELSE
+        RAISE NOTICE 'Opção inválida';
+        RETURN;
+    END IF;
+    RAISE NOTICE '% % % = %', op1, operador, op2, resultado;
+END
+$$;
+
+--CASE
+DO $$
+DECLARE
+    op1 INT := valor_aleatorio_entre(1,30);  
+    op2 INT := valor_aleatorio_entre(1,30);   
+    opcao INT := 3; -- (1: soma, 2: subtração, 3: multiplicação, 4: divisão)
+    resultado NUMERIC (10, 2);
+    operador TEXT;
+BEGIN
+    CASE 
+        WHEN opcao = 1 THEN
+            resultado := op1 + op2;
+            operador := '+';
+        WHEN opcao = 2 THEN
+            resultado := op1 - op2;
+            operador := '-';
+        WHEN opcao = 3 THEN
+            resultado := op1 * op2;
+            operador := '*';
+        WHEN opcao = 4 THEN
+            IF op2 = 0 THEN
+                RAISE NOTICE 'Erro: divisão por zero não é permitida';
+                RETURN;
+            END IF;
+            resultado := op1 / op2 ::FLOAT;
+            operador := '/';
+        ELSE
+            RAISE NOTICE 'Opção inválida';
+            RETURN;
+    END CASE;
+    RAISE NOTICE '% % % = %', op1, operador, op2, resultado;
+END
+$$;
+
+
 --1.2 Faça um programa que exibe se um número inteiro é múltiplo de 3 ou de 5.
 --IF
 DO $$
